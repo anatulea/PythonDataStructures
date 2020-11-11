940ec5f6b09de12d5701ef1aff97962b50c538ef
Ana Tulea <anatulea@Anas-MacBook-Pro.local>
2020-11-11 14:38:57 -0800

    string_compression

---
+++ src/arrays/02_string_compression.py
@@ -0,0 +1,42 @@
+def string_compression(s):
+    table = {}
+    result = []
+    for i in s:
+        if i not in table:
+             table[i] = 1
+        else:
+            table[i]+=1
+    
+    for k, v in table.items():
+        result.append(k)
+        result.append(str(v))  
+
+    return  ''.join(result)
+
+print(string_compression('AAAAAABBABEEEDDDdd')) #A7B3E3D3d2
+
+# run length compression algorithm
+def compress(s):
+    run = ""
+    length = len(s)
+
+    if length == 0:
+        return ''
+    
+    if length == 1:
+        return s+'1'
+    
+    last = s[0] 
+    count = 1
+    idx = 1
+
+    while idx < length:
+        if s[idx] == s[idx-1]:
+            count += 1
+        else:
+            run = run + s[idx-1]+str(count)
+            count = 1
+        idx += 1
+    run = run + s[idx-1] + str(count)
+    return run
+print(compress('AAAAAABBABEEEDDDdd')) # A6B2A1B1E3D3d2
